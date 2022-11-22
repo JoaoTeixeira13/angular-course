d09f2bbcb1adbe24fd525de40af7f491d8d0703b
João Teixeira <j.teixeira.dev@gmail.com>
2022-11-22 12:31:09 +0100

    standalone components routing

--- standalone-components-routing/src/app/app-routing.module.ts
+++ standalone-components-routing/src/app/app-routing.module.ts
@@ -1,29 +1,29 @@
-import { NgModule } from '@angular/core';
-import { Route, RouterModule } from '@angular/router';
-import { AboutComponent } from './about/about.component';
+import { NgModule } from "@angular/core";
+import { Route, RouterModule } from "@angular/router";
+import { AboutComponent } from "./about/about.component";
 
-import { WelcomeComponent } from './welcome/welcome.component';
+import { WelcomeComponent } from "./welcome/welcome.component";
 
 const routes: Route[] = [
-  {
-    path: '',
-    component: WelcomeComponent,
-  },
-  {
-    path: 'about',
-    component: AboutComponent,
-  },
-  {
-    path: 'dashboard',
-    loadChildren: () =>
-      import('./dashboard/dashboard-routing.module').then(
-        (mod) => mod.DashboardRoutingModule
-      ),
-  },
+    {
+        path: "",
+        component: WelcomeComponent,
+    },
+    {
+        path: "about",
+        // component: AboutComponent,
+        loadComponent: () =>
+            import("./about/about.component").then((mod) => mod.AboutComponent),
+    },
+    {
+        path: "dashboard",
+        loadChildren: () =>
+            import("./dashboard/routes").then((mod) => mod.DASHBOARD_ROUTES),
+    },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
 export class AppRoutingModule {}
