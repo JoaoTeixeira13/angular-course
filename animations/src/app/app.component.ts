371f991ef77bdd78744576795fd49f0fb534a400
João Teixeira <j.teixeira.dev@gmail.com>
2022-11-24 17:23:53 +0100

    animating transition states

--- animations/src/app/app.component.ts
+++ animations/src/app/app.component.ts
@@ -26,19 +26,58 @@ import { Component } from '@angular/core';
       transition('normal => highlighted', animate(300)),
       transition('highlighted => normal', animate(800)),
     ]),
+    trigger('wildState', [
+      state(
+        'normal',
+        style({
+          backgroundColor: 'red',
+          transform: 'translateX(0) scale(1)',
+          borderRadius: 0,
+        })
+      ),
+      state(
+        'highlighted',
+        style({
+          backgroundColor: 'blue',
+          transform: 'translateX(100px) scale(1)',
+          borderRadius: 0,
+        })
+      ),
+      state(
+        'shrunken',
+        style({
+          backgroundColor: 'green',
+          transform: 'translateX(0) scale(0.5)',
+          borderRadius: 0,
+        })
+      ),
+      transition('normal => highlighted', animate(300)),
+      transition('highlighted => normal', animate(800)),
+      transition('shrunken <=> *', [
+        style({ backgroundColor: 'orange' }),
+        animate(1000, style({ borderRadius: '50px' })),
+        animate(500),
+      ]),
+    ]),
   ],
 })
 export class AppComponent {
   list = ['Milk', 'Sugar', 'Bread'];
   state = 'normal';
+  wildState = 'normal';
 
   onAnimate() {
     this.state === 'normal'
       ? (this.state = 'highlighted')
       : (this.state = 'normal');
+
+    this.wildState === 'normal'
+      ? (this.wildState = 'highlighted')
+      : (this.wildState = 'normal');
   }
   onShrink() {
     console.log('item will shrink');
+    this.wildState = 'shrunken';
   }
 
   onAdd(item) {
