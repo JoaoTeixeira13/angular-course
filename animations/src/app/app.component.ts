1bfc92ccb0d64a42e86954273d3e5bb7ec5d126c
João Teixeira <j.teixeira.dev@gmail.com>
2022-11-24 16:46:08 +0100

    basic animation

--- animations/src/app/app.component.ts
+++ animations/src/app/app.component.ts
@@ -1,13 +1,47 @@
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  animations: [
+    trigger('divState', [
+      state(
+        'normal',
+        style({
+          backgroundColor: 'red',
+          transform: 'translateX(0)',
+        })
+      ),
+      state(
+        'highlighted',
+        style({ backgroundColor: 'blue', transform: 'translateX(100px)' })
+      ),
+      transition('normal => highlighted', animate(300)),
+      transition('highlighted => normal', animate(800)),
+    ]),
+  ],
 })
 export class AppComponent {
   list = ['Milk', 'Sugar', 'Bread'];
+  state = 'normal';
 
-    onAdd(item) {
-      this.list.push(item);
-    }
+  onAnimate() {
+    this.state === 'normal'
+      ? (this.state = 'highlighted')
+      : (this.state = 'normal');
+  }
+  onShrink() {
+    console.log('item will shrink');
+  }
+
+  onAdd(item) {
+    this.list.push(item);
+  }
 }
